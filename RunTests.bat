d832494b5407f39222aac523a02d321dfed64740
Mark Watson <markus.c.watson@gmail.com>
2022-12-26 10:20:27 -0400

    Fix indentations

--- RunTests.bat
+++ RunTests.bat
@@ -4,28 +4,28 @@ rmdir /s /q Testing
 
 :: Setup cmake and build source
 cmake 	--no-warn-unused-cli ^
-		-DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE ^
-		-DCMAKE_BUILD_TYPE:STRING=Debug ^
-		-DCMAKE_C_COMPILER:FILEPATH=C:\mingw64\bin\gcc.exe ^
-		-DCMAKE_CXX_COMPILER:FILEPATH=C:\mingw64\bin\g++.exe ^
-		-DTESTING=TRUE ^
-		-S . ^
-		-B ./build ^
-		-G "MinGW Makefiles" 
+	-DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE ^
+	-DCMAKE_BUILD_TYPE:STRING=Debug ^
+	-DCMAKE_C_COMPILER:FILEPATH=C:\mingw64\bin\gcc.exe ^
+	-DCMAKE_CXX_COMPILER:FILEPATH=C:\mingw64\bin\g++.exe ^
+	-DTESTING=TRUE ^
+	-S . ^
+	-B ./build ^
+	-G "MinGW Makefiles" 
 
 :: Build UnitTesting project
 cmake 	--build ./build ^
-		--config Debug ^
-		--target all ^
-		-j 14 
+	--config Debug ^
+	--target all ^
+	-j 14 
 
 :: Execute tests
 ctest 	-j 12 ^
-		-C Debug ^
-		--test-dir ./build/test ^
-		--extra-verbose ^
-		--progress ^
-		--output-on-failure
+	-C Debug ^
+	--test-dir ./build/test ^
+	--extra-verbose ^
+	--progress ^
+	--output-on-failure
 
 pause
 :: exit /B 0
